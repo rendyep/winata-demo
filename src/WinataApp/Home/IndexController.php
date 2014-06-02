f51656d1717c90c8e069b6ff1cad8dcd8327b0c2
rendyep <rendyekoprastiyo@gmail.com>
2014-06-02 15:51:34 +0700

    add licence in each file

--- src/WinataApp/Home/IndexController.php
+++ src/WinataApp/Home/IndexController.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+* Winata Demo (https://github.com/rendyep/winata)
+*
+* @link https://github.com/rendyep/winata-demo for the canonical source repository
+* @copyright Copyright (c) 20014 Winata Technology Inc. (https://github.com/rendyep/winata)
+* @license https://github.com/rendyep/winata-demo/blob/master/LICENCE
+*/
+
 namespace WinataApp\Home;
 
 use Winata\Mvc\Controller\AbstractActionController;
